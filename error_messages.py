b461ae1b5b735c1af2093aace736baa6b905669d
minhnd <minhnd@gigatum.com>
2019-07-10 14:05:43 +0700

    add new table

--- error_messages.py
+++ error_messages.py
@@ -15,3 +15,4 @@ follower_not_found={'message':"Người dùng không tồn tại", 'error_code':
 following_yourself={'message':"Bạn không thể tự theo dõi chính mình", 'error_code':1002, 'status_code': 400}
 followed_user={'message':"Bạn đã theo dõi người này", 'error_code':1002, 'status_code': 400}
 story_not_found={'message':"Câu chuyện không tồn tại", 'error_code':1002, 'status_code': 400}
+action_404_not_found={'message':"Câu chuyện không tồn tại", 'error_code':44044, 'status_code': 404}
